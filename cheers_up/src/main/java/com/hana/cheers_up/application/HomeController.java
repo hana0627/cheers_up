5ed6ddb873bdd4ce36e4195f0be6d2f0f976a424
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-04-16 17:56:44 +0900

    #6 루트경로 컨트롤러 추가
    처음 페이지를 접속했을때, 안내하는 컨트롤러를 생성.
    회원가입 성공시 해당 뷰로 이동시키는 것으로 설계함

---
+++ cheers_up/src/main/java/com/hana/cheers_up/application/HomeController.java
@@ -0,0 +1,13 @@
+package com.hana.cheers_up.application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "search_page";
+    }
+}
