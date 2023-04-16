f239041d983d741283bfc87be752828c2676f707
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-04-16 14:10:49 +0900

    #1-firstcommit
    스프링부트를 이용하여 프로젝트를 생성하였다.

---
+++ cheers_up/src/main/java/com/hana/cheers_up/CheersUpApplication.java
@@ -0,0 +1,13 @@
+package com.hana.cheers_up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheersUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheersUpApplication.class, args);
+    }
+
+}
