ecc5fd80b4893267400ddf8e82b274bb08b7019e
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-04-23 18:24:10 +0900

    #30 - jwt 기능 구현 그러나..
    jwt를 이용하여 토큰발급까지는 해결하였으나..... 
    허점투성이인것 같다...
    값진 경험이였다고 생각하자.

--- cheers_up/src/main/java/com/hana/cheers_up/global/security/KakaoOAuth2Response.java
+++ cheers_up/src/main/java/com/hana/cheers_up/global/security/KakaoOAuth2Response.java
@@ -5,7 +5,7 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
-@SuppressWarnings("unchecked") // TODO: Map -> Object 변환 로직이 있어 제네릭 타입 캐스팅 문제를 무시한다. 더 좋은 방법이 있다면 고려할 수 있음.
+@SuppressWarnings("unchecked")
 public record KakaoOAuth2Response(
         Long id,
         LocalDateTime connectedAt,
